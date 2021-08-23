b9e87c625a89be5bce0733e17da3f13089199398
thuongvb <baothuong9608@gmail.com>
2021-08-23 16:10:35 +0700

    create product component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-function App() {
-  return (
-    <div className="grid-container">
-      <header className="App-header">
-        <a href="/" className="">
-          React Shopping Cart
-        </a>
-      </header>
-      <main>ProductList</main>
-      <footer>Copyright &copy;</footer>
-    </div>
-  );
+import data from "./data.json";
+import React from "react";
+import Products from "./components/Products/Products";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header className="App-header">
+          <a href="/" className="">
+            React Shopping Cart
+          </a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>Copyright &copy;</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
