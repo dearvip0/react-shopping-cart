888c64e6fcbbdb85112f0255f71d99fd3f067947
thuongvb <baothuong9608@gmail.com>
2021-08-24 21:06:50 +0700

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import data from "./data.json";
 import React from "react";
 import Products from "./components/Products/Products";
+import Filter from "./components/Filter/Filter";
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       products: data.products,
       size: "",
       sort: "",
     };
   }
+
+  sortProducts = (event) => {
+    //implements
+    console.log(event.target.value);
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
 
   render() {
     return (
@@ -22,6 +60,13 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">Cart Items</div>
